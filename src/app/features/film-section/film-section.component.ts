cb984cc61206dee3428dcdbae541919a967bab31
VictorGonPe <victor.gonzalez.perez.1982@gmail.com>
2025-05-23 16:26:27 +0200

    feat: show films preview

--- src/app/features/film-section/film-section.component.ts
+++ src/app/features/film-section/film-section.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal, inject, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { environment } from '../../../../environment';
 
 @Component({
   selector: 'app-film-section',
@@ -7,6 +10,18 @@ import { Component } from '@angular/core';
   templateUrl: './film-section.component.html',
   styleUrl: './film-section.component.css'
 })
-export class FilmSectionComponent {
 
+export class FilmSectionComponent {
+  private http = inject(HttpClient);
+  private apiUrl = `https://api.themoviedb.org/3/movie/top_rated?api_key=${environment.TMDB_API_KEY}`;
+
+  // Observable → Signal con toSignal()
+  movies = toSignal(
+    this.http.get<{ results: any[] }>(this.apiUrl),
+    { initialValue: { results: [] } }
+  );
+
+  get results() {
+    return this.movies().results;
+  }
 }
