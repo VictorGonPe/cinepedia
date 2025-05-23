cf7a91fbc243a248b0eceb6f0e494d0b134e653c
VictorGonPe <victor.gonzalez.perez.1982@gmail.com>
2025-05-23 15:46:01 +0200

    feat: add structure and first components

---
+++ src/app/features/film-section/film-section.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-film-section',
+  imports: [],
+  templateUrl: './film-section.component.html',
+  styleUrl: './film-section.component.css'
+})
+export class FilmSectionComponent {
+
+}
