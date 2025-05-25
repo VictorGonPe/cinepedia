771612aadf069d91a28fd0f01a5bbc8880d00a25
VictorGonPe <victor.gonzalez.perez.1982@gmail.com>
2025-05-25 14:43:28 +0200

    feat: add social component

--- src/app/shared/layout/header/header.component.ts
+++ src/app/shared/layout/header/header.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { LogoComponent } from "../../ui/logo/logo.component";
+import { SocialComponent } from "../../ui/social/social.component";
 
 @Component({
   selector: 'app-header',
-  imports: [RouterModule, LogoComponent],
+  imports: [RouterModule, LogoComponent, SocialComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
