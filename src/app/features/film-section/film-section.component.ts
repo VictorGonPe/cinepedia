3e8ce4f3f7c9962fb0bf061a7ae01ac251a4d626
VictorGonPe <victor.gonzalez.perez.1982@gmail.com>
2025-05-27 22:04:55 +0200

    feat: add detail view and service

--- src/app/features/film-section/film-section.component.ts
+++ src/app/features/film-section/film-section.component.ts
@@ -1,11 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { FilmService } from '../../core/services/film.service';
-
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-film-section',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './film-section.component.html',
   styleUrl: './film-section.component.css'
 })
