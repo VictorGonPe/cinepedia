7f8c83e467b1ccc4ef992b1b536fb575f57c7b01
VictorGonPe <victor.gonzalez.perez.1982@gmail.com>
2025-05-29 11:48:10 +0200

    feat: add page button in film-section

--- src/app/features/film-section/film-section.component.ts
+++ src/app/features/film-section/film-section.component.ts
@@ -1,6 +1,8 @@
-import { Component, inject } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
 import { FilmService } from '../../core/services/film.service';
 import { RouterModule } from '@angular/router';
+import { toSignal, toObservable } from '@angular/core/rxjs-interop';
+import { switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-film-section',
@@ -9,8 +11,29 @@ import { RouterModule } from '@angular/router';
   templateUrl: './film-section.component.html',
   styleUrl: './film-section.component.css'
 })
-
 export class FilmSectionComponent {
   private filmService = inject(FilmService);
-  dataMovies = this.filmService.rateMovies;
+
+  page = signal(1);
+
+  private page$ = toObservable(this.page);
+
+  private allMovies$ = this.page$.pipe(
+    switchMap((page: number) => this.filmService.getMoviesByPage(page))
+  );
+
+
+  allMovies = toSignal(this.allMovies$, {
+    initialValue: { results: [] }
+  });
+
+  dataMovies = computed(() => this.allMovies().results);
+
+  nextPage() {
+    this.page.update(p => p + 1);
+  }
+
+  prevPage() {
+    this.page.update(p => (p > 1 ? p - 1 : 1));
+  }
 }
