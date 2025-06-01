b5910ffaad27738703c12d1da24de477d692f8b7
VictorGonPe <victor.gonzalez.perez.1982@gmail.com>
2025-06-01 20:32:59 +0200

    feat:when you log in or register, you return to the home screen with the registered user.

--- src/app/views/register/register.component.ts
+++ src/app/views/register/register.component.ts
@@ -4,11 +4,12 @@ import { AuthService } from '../../core/services/auth.service';
 import { FormsModule } from '@angular/forms';
 import { ButtonComponent } from '../../shared/ui/button/button.component';
 import { LogoComponent } from "../../shared/ui/logo/logo.component";
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [CommonModule, FormsModule, ButtonComponent, LogoComponent],
+  imports: [CommonModule, FormsModule, ButtonComponent, LogoComponent, RouterModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
@@ -21,7 +22,7 @@ export class RegisterComponent {
 
   onSubmit() {
     this.auth.register(this.email, this.password)
-      .then(() => console.log('✅ Registrado correctamente'))
+      .then(() => window.location.href = '')
       .catch(err => {
         if (err.code === 'auth/email-already-in-use') {
           this.error.set('⚠️ Este correo ya está registrado.');
