b6f0684a198a8a0fc376922495c75aa89fe48a00
VictorGonPe <victor.gonzalez.perez.1982@gmail.com>
2025-06-01 14:54:23 +0200

    Add Login and Register components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,16 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './views/home/home.component';
 import { FilmsComponent } from './views/films/films.component';
-import { NotFoundComponent } from './views/not-found/not-found.component';
 import { DetailFilmComponent } from './features/detail-film/detail-film.component';
+import { LoginComponent } from './views/login/login.component';
+import { RegisterComponent } from './views/register/register.component';
+
 
 export const routes: Routes = [
-    {path: '', component: HomeComponent},
-    {path: 'films', component: FilmsComponent},
-    {path: 'films/:id', component: DetailFilmComponent},
-    {path: '**', component: NotFoundComponent}
+    { path: '', component: HomeComponent },
+    { path: 'films', component: FilmsComponent },
+    { path: 'films/:id', component: DetailFilmComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent },
+    { path: '**', redirectTo: 'login' }
 ];
