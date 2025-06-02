890a812b3dbcf0ea1fb4fc9d8d7442ad9c1ba9f8
VictorGonPe <victor.gonzalez.perez.1982@gmail.com>
2025-06-02 21:00:06 +0200

    feat: add movies by actor

--- src/app/shared/ui/card/card.component.ts
+++ src/app/shared/ui/card/card.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { CastMember } from '../../../core/models/film.model';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './card.component.html',
   styleUrl: './card.component.css'
 })
