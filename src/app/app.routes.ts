1a8d5c73c4374aa81ead3c314d60c493cfc9eff6
VictorGonPe <victor.gonzalez.perez.1982@gmail.com>
2025-06-02 21:00:06 +0200

    feat: add movies by actor

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,7 @@ import { LoginComponent } from './views/login/login.component';
 import { RegisterComponent } from './views/register/register.component';
 import { LayoutComponent } from './views/layout/layout.component';
 import { authGuard } from './core/guards/auth.guard';
+import { FilmsByIdComponent } from './features/films-by-id/films-by-id.component';
 
 
 export const routes: Routes = [
@@ -14,7 +15,9 @@ export const routes: Routes = [
         children: [
             { path: '', component: HomeComponent },
             { path: 'films', component: FilmsComponent },
-            { path: 'films/:id', component: DetailFilmComponent, canActivate: [authGuard] },
+            // { path: 'films/:id', component: DetailFilmComponent, canActivate: [authGuard] },
+            { path: 'films/:id', component: DetailFilmComponent },
+            { path: 'actor/:id', component: FilmsByIdComponent }
         ]
     },
 
--- src/app/features/films-by-id/films-by-id.component.ts
+++ src/app/features/films-by-id/films-by-id.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, inject, signal } from '@angular/core';
+import { Component, computed, effect, inject, signal } from '@angular/core';
 import { FilmService } from '../../core/services/film.service';
 import { MovieCastCredit } from '../../core/models/film.model';
 import { ActivatedRoute } from '@angular/router';
@@ -7,6 +7,7 @@ import { catchError, filter, of, switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-films-by-id',
+  standalone: true,
   imports: [],
   templateUrl: './films-by-id.component.html',
   styleUrl: './films-by-id.component.css'
@@ -15,16 +16,26 @@ export class FilmsByIdComponent {
   private route = inject(ActivatedRoute);
   private filmService = inject(FilmService);
 
-  private paramMap = toSignal(this.route.paramMap, { initialValue: this.route.snapshot.paramMap });
+  private paramMap = toSignal(this.route.paramMap, {
+    initialValue: this.route.snapshot.paramMap
+  });
 
   actorID = computed(() => Number(this.paramMap().get('id') || NaN));
-  films = signal<MovieCastCredit[]>([]);
+  actorMovies = signal<MovieCastCredit[]>([]);
+  actorName = signal<string>('');
 
-  actorMovies = toSignal(
-    this.filmService.getMovieByPerson(this.actorID()).pipe(
-      catchError(() => of([]))
-    ),
-    { initialValue: [] }
-  );
 
+  constructor() {
+    effect(() => {
+      const id = this.actorID();
+      if (!isNaN(id)) {
+        this.filmService.getMovieByPerson(id).subscribe({
+          next: (res) => this.actorMovies.set(res),
+          error: () => this.actorMovies.set([])
+        });
+      }
+    });
+    const queryParams = inject(ActivatedRoute).snapshot.queryParamMap;
+    this.actorName.set(queryParams.get('name') || 'Unknown');
+  }
 }
