5a5887b514c3ce8feddc361f23c6496e23f1163a
VictorGonPe <victor.gonzalez.perez.1982@gmail.com>
2025-06-03 21:22:48 +0200

    feat: delete jest and instal karma + jasmine

--- src/app/shared/ui/card/card.component.spec.ts
+++ src/app/shared/ui/card/card.component.spec.ts
@@ -1,24 +1,32 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { CardComponent } from './card.component';
-
-
+import { provideRouter } from '@angular/router';
+import { CastMember } from '../../../core/models/film.model';
 
 describe('CardComponent', () => {
-    let component: CardComponent;
-    let fixture: ComponentFixture<CardComponent>;
+  let component: CardComponent;
+  let fixture: ComponentFixture<CardComponent>;
 
-    beforeEach(async () => {
-        await TestBed.configureTestingModule({
-            imports: [CardComponent]
-        })
-            .compileComponents();
+  const actor: CastMember = {
+    id: 1,
+    name: 'John Doe',
+    character: 'Hero',
+    profile_path: '/profile.jpg'
+  };
 
-        fixture = TestBed.createComponent(CardComponent);
-        component = fixture.componentInstance;
-        fixture.detectChanges();
-    });
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [CardComponent],
+      providers: [provideRouter([])],
+    }).compileComponents();
 
-    it('should create', () => {
-        expect(component).toBeTruthy();
-    });
+    fixture = TestBed.createComponent(CardComponent);
+    component = fixture.componentInstance;
+    component.actor = actor; // importante para que @Input no sea undefined
+    fixture.detectChanges();
+  });
+
+  it('should create the component', () => {
+    expect(component).toBeTruthy();
+  });
 });
